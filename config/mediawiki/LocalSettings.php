3793504cb1ad17184c96b738b754780bff7b5f8f
Kevin Aguiar <kevin.aguiar@credija.com.br>
2018-12-05 08:20:47 -0200

    FIX: Postgresql support

--- config/mediawiki/LocalSettings.php
+++ config/mediawiki/LocalSettings.php
@@ -70,7 +70,7 @@ if (getenv('MEDIAWIKI_DB_PASSWORD') != '') {
 }
 
 # MySQL/Postgresql specific settings
-if (getenv('MEDIAWIKI_DB_TYPE') == 'mysql' || getenv('MEDIAWIKI_DB_TYPE') == 'postgresql') {
+if (getenv('MEDIAWIKI_DB_TYPE') == 'mysql' || getenv('MEDIAWIKI_DB_TYPE') == 'pgsql') {
     // Cache sessions in database
     $wgSessionCacheType = CACHE_DB;
 
